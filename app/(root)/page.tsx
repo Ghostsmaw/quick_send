77209e1533994f051f4ccdc07b63002b68e923f0
kingsley c. amaku <kingsleyc.amaku@kingsleys-MacBook-Pro-2.local>
2025-02-05 02:40:35 +0100

    Layout development

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import HeaderBox from "@/components/HeaderBox";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
+import React from "react";
 
 const Home = () => {
-  return (
-    <div>
-      HOME
-    </div>
-  )
-}
+  const loggedIn = { firstname: "Johnathan" };
 
-export default Home
+  return (
+    <section className="home">
+      <div className="home-content">
+        <HeaderBox
+          type="greeting"
+          title="Welcome"
+          user={loggedIn?.firstname || "guest"}
+          subtext="Access and Manage your transactions"
+        />
+
+        <TotalBalanceBox 
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250}  // Replace with actual total balance from API call or state variable. 10000 is just a placeholder. 10000 = $10,000.00
+        />
+      </div>
+    </section>
+  );
+};
+
+export default Home;
