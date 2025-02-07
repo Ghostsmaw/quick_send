c259971fa0f12b1ccd20b20b1186efc9bddd9d60
ghostsmaw <ghostsmaw@gmail.com>
2025-02-07 21:49:14 +0100

    mobile menu

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -1,19 +1,96 @@
+"use client";
 import {
-    Sheet,
-    SheetContent,
-    SheetDescription,
-    SheetHeader,
-    SheetTitle,
-    SheetTrigger,
-  } from "@/components/ui/sheet"
-  
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { sidebarLinks } from "@/constants";
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
-const MobileNav = ({user} : MobileNavProps) => {
+const MobileNav = ({ user }: MobileNavProps) => {
+  const pathName = usePathname();
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <section className="w-full max-w-[246px]">
+      <Sheet>
+        <SheetTrigger>
+          <Image
+            src="/icons/hamburger.svg"
+            alt="menu"
+            width={30}
+            height={30}
+            className="cursor-pointer"
+          />
+        </SheetTrigger>
+        <SheetContent side="left" className="border-none bg-white">
+          <Link
+            href="/"
+            className="flex cursor-pointer items-center gap-2 gap-1 px-4"
+          >
+            <Image
+              src="/icons/logo.svg"
+              width={34}
+              height={34}
+              alt="Quick Send Logo"
+            />
+            <h1 className="text-26 font-ibm-plex-serif font-bold text-black-1">
+              QuickSend
+            </h1>
+          </Link>
+          <div className="mobilenav-sheet">
+            <SheetClose asChild>
+              <nav className="flex h-full flex-col gap-6 pt-16 text-white">
+                {sidebarLinks.map((item) => {
+                  const isActive =
+                    pathName === item.route ||
+                    pathName.startsWith(`${item.route}/`);
+                  return (
+                    <SheetClose asChild key={item.route}>
+                      <Link
+                        href={item.route}
+                        key={item.label}
+                        className={cn("mobilenav-sheet_close w-full", {
+                          "bg-bank-gradient": isActive,
+                        })}
+                      >
+                        <Image
+                          alt={item.label}
+                          src={item.imgURL}
+                          width={20}
+                          height={20}
+                          className={cn({
+                            "brightness-[3] invert-0": isActive,
+                          })}
+                        />
+                        <p
+                          className={cn("text-16 font-semibold text-black-2", {
+                            "!text-white": isActive,
+                          })}
+                        >
+                          {item.label}
+                          {isActive && (
+                            <span className="absolute bottom-0 right-0 ml-2 text-xs text-white bg-indigo-500 p-1 rounded-full">
+                              Active
+                            </span>
+                          )}
+                        </p>
+                      </Link>
+                    </SheetClose>
+                  );
+                })}
+              </nav>
+            </SheetClose>
+          </div>
+        </SheetContent>
+      </Sheet>
+    </section>
+  );
+};
 
-export default MobileNav
+export default MobileNav;
