a384a2dc08c17679253d86de43c34502f3ced31d
ghostsmaw <ghostsmaw@gmail.com>
2025-02-08 20:36:51 +0100

    Right Sidebar

--- components/RightSideBar.tsx
+++ components/RightSideBar.tsx
@@ -2,8 +2,15 @@ import React from 'react'
 
 const RightSideBar = ({user, transactions, banks}: RightSidebarProps) => {
   return (
-    <aside>
-      
+    <aside className='right-sidebar'>
+      <section className='flex flex-col pb-8'>
+        <div className='profile-banner'/>
+        <div className='profile'>
+          <div className='profile-img'>
+            <span className='text-5xl font-bold'>{user.firstname[0]}</span>
+          </div>
+        </div>
+      </section>
     </aside>
   )
 }
