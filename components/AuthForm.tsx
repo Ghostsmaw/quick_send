d7b35b4aff5c663fdec5a4ac1e7d22385b47fd1c
ghostsmaw <ghostsmaw@gmail.com>
2025-02-11 23:48:32 +0100

    authform

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const AuthForm = ({type}: {type: string}) => {
   return (
-    <div>{type}</div>
+    <section className='auth-form'>
+        <header className=''>
+
+        </header>
+    </section>
   )
 }
 
