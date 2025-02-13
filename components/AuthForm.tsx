65585e2b7d41d227d07bb560e30b7a5503bcba48
ghostsmaw <ghostsmaw@gmail.com>
2025-02-13 23:33:15 +0100

    Authform

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -1,3 +1,4 @@
+'use client';
 import Link from 'next/link'
 import React, {useState} from 'react'
 import Image from "next/image";
@@ -23,8 +24,13 @@ const AuthForm = ({type}: {type: string}) => {
           </Link>
 
           <div className='flex flex-col gap-1 md:gap-3'>
-            <h1>
+            <h1 className='text-24 lg:text-36 font-semibold text-gray-900'>
                 {user ? "Link Account" : type === "sign-in" ? "Sign In" : "Sign Up"}
+                <p className='text-16 font-normal text-gray-600'>
+                  {
+                    user ? "Link your account to get started" : "Please enter your details"
+                  }
+                </p>
             </h1>
           </div>
         </header>
